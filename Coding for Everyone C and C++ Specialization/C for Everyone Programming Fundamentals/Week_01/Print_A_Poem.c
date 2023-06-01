0f1ce3fbf805c3df2093e36bfdb8b1f0277a3e8f
GautamiShetty <95223703+GautamiShetty@users.noreply.github.com>
2023-06-01 13:57:02 +0530

    Add files via upload

---
+++ Coding for Everyone C and C++ Specialization/C for Everyone Programming Fundamentals/Week_01/Print_A_Poem.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("\nWhat happens to a dream deferred?\n\nDoes it dry up\nlike a raisin in the sun?\nOr fester like a sore-\n");
+    printf("And then run?\nDoes it stink like rotten meat?\nOr crust and sugar over-\nlike a syrupy sweet?\n\n"); 
+    printf("Maybe it just sags\nlike a heavy load.\n\nOr does it explode ?\n"); 
+    
+    return 0;
+}
